c27ce14d4aff538380e00120bf2b2d4bf6b6a9c7
DeonDp <deongohard@hotmail.com>
2016-10-10 16:54:10 -0700

    excercise 3

--- maze.js
+++ maze.js
@@ -1,17 +1,31 @@
 window.onload =function handler() {
 
-	var walls =document.querySelectorAll('div.boundary');
+	var win =document.getElementById('end');
+	var walls =document.querySelectorAll('#maze div.boundary');
 
 	for (var i =0; i <=4; i++) {
 		walls[i].setAttribute('onmouseover','testTraversal()');
 	}
+
+	win.setAttribute('onmouseover','victory()');
 };
 
 function testTraversal() {
+	
+	alert('You Lose!');
 	var walls =document.querySelectorAll('div.boundary');
 
 	for (var i =0; i <=4; i++) {
 		walls[i].setAttribute('style','background-color:#ff8888');
+	}	
+}
+
+function victory() {
+
+	if(document.getElementById('boundary1').getAttribute('style') == 'background-color:#ff8888') {
+		alert('You must go through the maze \nwithout hitting the walls!');
+	}	
+	else {
+		alert('You Win!');
 	}
-	
 }
